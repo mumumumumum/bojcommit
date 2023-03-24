2f420a8dee082a386f37a5e2d9e6b26f0be9bd72
mumumumumum <128616433+mumumumumum@users.noreply.github.com>
2023-03-24 23:35:23 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+vector<vector<int> > RobotMap(51,vector<int>(51));
+vector<vector<bool> > visited(51,vector<bool>(51,false));
+int dx[4]={-1,0,1,0};
+int dc[4]={0,1,0,-1};
+int n,m,r,c,d,cnt=0;
+void dfs(int row,int col,int Direction){
+    if(!visited[row][col]){
+        cnt++;
+        visited[row][col]=true;
+    }
+    //cout<<row<<" "<<col<<" "<<Direction<<" "<<cnt<<"\n";
+   
+    bool check=false;
+    for(int i=0;i<4;i++){
+        //int newDirection=(Direction-1<0) ? Direction=3 : Direction-1;
+        if(Direction-1<0)Direction=3;
+        else Direction--;
+        
+        int r=row+dx[Direction];
+        int c=col+dc[Direction];
+
+        if(r<0||r>=n||c<0||c>=m)continue;
+        if(visited[r][c]||RobotMap[r][c]==1)continue;
+        check=true;
+        dfs(r,c,Direction);
+        
+    }
+    if(!check){
+        int newDirection = (Direction+2 > 3) ? Direction-2 : Direction+2;
+        int r=row+dx[newDirection];
+        int c=col+dc[newDirection];
+        //cout<<RobotMap[r][c]<<"\n";
+        //if(r<0||r>=n||c<0||c>=m) return;
+        if(RobotMap[r][c]==1) {
+            cout<<cnt;
+            exit(0);
+        }
+        else dfs(r,c,Direction);
+
+    }
+}
+
+int main(void)
+{
+    vector<int> v;
+    
+    cin>>n>>m>>r>>c>>d;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>RobotMap[i][j];
+        }
+    }
+    dfs(r,c,d);
+    
+    return 0;
+}
