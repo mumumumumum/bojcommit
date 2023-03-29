173c0e92490ff36732a68a60ec99abf2039a63ee
mumumumumum <128616433+mumumumumum@users.noreply.github.com>
2023-03-29 19:18:08 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+
+
+
+
+int main(void)
+{
+    int n;
+    cin>>n;
+    cout<<n/25+n/5+(n/25)/5;
+    return 0;
+}
